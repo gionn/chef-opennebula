0bc666e453d48eb497f0840667dbf436a9f4cef5
Giovanni Toraldo <me@gionn.net>
2013-03-18 23:57:55 +0100

    Added sqlite3 cli tool

--- recipes/build-dep.rb
+++ recipes/build-dep.rb
@@ -1,5 +1,5 @@
 
-%w{scons libsqlite3-dev libxmlrpc-c3-dev libxml2-dev libxslt1-dev libssl-dev libcurl4-openssl-dev libmysqlclient-dev rake}.each do |pkg|
+%w{scons libsqlite3-dev libxmlrpc-c3-dev libxml2-dev libxslt1-dev libssl-dev libcurl4-openssl-dev libmysqlclient-dev rake sqlite3}.each do |pkg|
     package pkg do
         action :install
     end
